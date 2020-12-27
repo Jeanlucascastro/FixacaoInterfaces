7a362c935a9b4f5e19e3c1333b84e5b174c248f9
Jean Castro <jeanlucascastro@gmail.com>
2020-12-27 12:30:18 -0300

    Finalizado

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,16 +1,44 @@
 package Application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Contract;
+import entities.parcelas;
+import services.ContratoServico;
+import services.PaypalService;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy"); 
 		
 		
+		System.out.println("Digite o numero do contrato ");
+		int numero = sc.nextInt();
+		System.out.println("Digite a data do contrato ");
+		Date data = sdf.parse(sc.next());
+		System.out.println("Valor Total do contrato");
+		double valorTotal = sc.nextDouble();
 		
+		Contract contract = new Contract(numero,data,valorTotal);
+		
+		System.out.println("Digite a quantidade de parcelas ");
+		int n = sc.nextInt();
+		
+		ContratoServico contratoservico = new ContratoServico(new PaypalService());
+		
+		contratoservico.processarContrato(contract, n);
+		
+		System.out.println("Parcelas");
+		for (parcelas x : contract.getParcelas()) {
+			System.out.println(x);
+		}
 		
 		
 		
